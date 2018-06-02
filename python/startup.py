4f0213dfe6cacf5b65ade9cc3b5a77f361a3c4fd
Georg vom Endt <gve@3yourmind.com>
2018-06-03 00:47:19 +0200

    added lost python sons

---
+++ python/startup.py
@@ -0,0 +1,15 @@
+import os
+
+def run():
+    command = 'silent new_attr bool my_not_first_autostart'
+    exitcode = os.system('herbstclient ' + command)
+
+    if exitcode == 0:
+        #os.system("compton &")
+        os.system("dunst &")
+        os.system("parcellite &")
+        os.system("nitrogen --restore &")
+        os.system('xli -border black -onroot -display ":0" -fillscreen  ~/.config/herbstluftwm/cat_pic.jpg')
+        os.system("mpd &")
+        os.system("redshift &")
+
